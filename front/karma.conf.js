d5a384a83b9461e5b815395473a2fced56d32c66
romainchavalle <165096334+romainchavalle@users.noreply.github.com>
2025-11-09 23:45:29 +0100

    Update karma.conf.js

--- front/karma.conf.js
+++ front/karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcov', subdir: '.'  },
       ]
     },
     reporters: ['progress', 'kjhtml'],
